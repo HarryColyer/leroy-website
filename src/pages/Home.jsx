2fcb9688a91ab4d6e3e65d7ce76cfdece256be2c
Harryc <harrycolyer@hotmail.co.uk>
2024-10-16 00:01:06 +0100

    updated: home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,29 +4,30 @@ function Home() {
   return (
     <div>
       <header className="roboto-font text-white w-full lg:max-h-[450px] xl:max-h-[600px] overflow-hidden grid grid-cols-2 grid-rows-6 lg:grid-rows-2 lg:grid lg:grid-cols-5">
-        <div className="roboto-font bg-red-800 py-5 lg:bg-black w-full order-2 lg:order-1 col-span-2 row-span-3 lg:col-start-1 lg:col-end-4 flex flex-col lg:py-6 px-12">
-          <h2 className="text-3xl " >Why Replace When You Can Restore?</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Repellendus sit soluta consequatur odit impedit sed aliquam
-            perferendis earum perspiciatis officiis, enim laboriosam accusantium
-            adipisci quidem minima reiciendis nisi. Corrupti, iure?
+        <div className="roboto-font bg-red-800 py-16 px-12 md:px-20 w-full order-2 lg:order-1 col-span-2 row-span-3 lg:col-start-1 lg:col-end-4 flex flex-col lg:py-6 ">
+          <h2 className="text-2xl py-4 ">Why Replace? When You Can Restore.</h2>
+          <p className="text-base py-3">
+            At Scratch and Dent Repairs, we provide professional surface
+            restoration services, delivering high-quality repairs that restore
+            surfaces to their original condition. Our expertise helps you avoid
+            the expense of replacement while ensuring excellent results.
           </p>
-          <ul>
-            <li>Wood & laminates</li>
-            <li>Baths, sinks & showers</li>
-            <li>Brick & stone</li>
-            <li>Glass polishing</li>
-            <li>Wall & floor tiles</li>
-            <li>uPVC & aluminum</li>
-            <li>Kitchen surfaces</li>
+          <ul className="pt-2 pl-4 space-y-1 grid grid-cols-2 font-bold">
+            <li>Woods</li>
+            <li>Laminates</li>
+            <li>Baths, Sinks & Showers</li>
+            <li>Brick & Stone</li>
+            <li>Glass Polishing</li>
+            <li>Wall & Floor tiles</li>
+            <li>uPVC</li>
+            <li>Kitchen Surfaces</li>
           </ul>
         </div>
 
-        <div className="relative order-1 lg:order-2 h-auto sm:h-[350px] lg:max-h-[450px] w-full col-span-2 row-span-3 lg:row-span-4 lg:col-start-4 lg:col-end-6">
+        <div className="relative order-1 sm:h-[450px] lg:order-2 h-auto lg:max-h-[450px] w-full col-span-2 row-span-3 lg:row-span-4 lg:col-start-4 lg:col-end-6">
           {/* Image itself */}
           <img
-            className="w-full h-full object-cover"
+            className="w-full h-full inset-0 object-cover"
             src={headerImg}
             alt="header image of a bathroom"
           />
